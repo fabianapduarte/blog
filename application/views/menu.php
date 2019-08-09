4d10c4091016263203f6452d934985bd0f95ab74
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-08-09 00:29:08 -0300

    adição do bootstrap e criação do menu

---
+++ application/views/menu.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+		<!-- JQuery and Popper.js -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<!-- Bootstrap JavaScript -->
+		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+		<!-- Fonte -->
+
+
+		<title>My Blog - Página Inicial</title>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light py-3 px-5">
+		  <a class="navbar-brand" href="#">
+		    <img src="<?php echo base_url(); ?>assets/img/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+		    My Blog
+		  </a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+		    <ul class="navbar-nav">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Home</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Posts</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Categorias</a>
+		      </li>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          <img src="<?php echo base_url(); ?>assets/img/user.png" width="30" height="30" class="d-inline-block align-top" alt="">
+		        </a>
+		        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+		          <a class="dropdown-item" href="#">Meu perfil</a>
+		          <a class="dropdown-item" href="#">Sair</a>
+		        </div>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
