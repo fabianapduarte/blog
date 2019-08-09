bdcceee735132e3194fc08b58fc9101f0d277002
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-08-09 14:17:55 -0300

    alteração no conteúdo html

--- application/views/content.php
+++ application/views/content.php
@@ -1,37 +1,42 @@
 		<div class="container">
-			<div class="display-4 my-5">Novas publicações</div>
+			<div class="row justify-content-between">
+				<div class="col-7">
+					<div class="display-4 my-5">Novas publicações</div>
 
-			<div class="card-group">
-				<div class="card">
-					<!--<img class="card-img-top" src="<?php echo base_url(); ?>assets/img/img1.jpg" alt="Imagem de capa do card">-->
-					<div class="card-body">
-						<?php foreach ($post as $p) {?>
-						<h5 class="card-title"><?php echo $p->titulo; ?></h5>
-						<p class="card-text"><?php echo $p->descricao; ?></p>
-						<p class="card-text"><small class="text-muted"><?php echo $p->datacadastro; ?></small></p>
-						<?php } ?>
+					<div class="card-group">
+						<div class="card">
+							<!--<img class="card-img-top" src="<?php echo base_url(); ?>assets/img/img1.jpg" alt="Imagem de capa do card">-->
+							<div class="card-body"> <?php foreach ($post as $p) {?>
+								<h5 class="card-title"><?php echo $p->titulo; ?></h5>
+								<p class="card-text"><?php echo $p->descricao; ?></p>
+								<p class="card-text"><small class="text-muted"><?php echo $p->datacadastro; ?></small></p> <?php } ?>
+							</div>
+						</div>
+						<div class="card">
+							<!--<img class="card-img-top" src="<?php echo base_url(); ?>assets/img/img1.jpg" alt="Imagem de capa do card">-->
+							<div class="card-body"> <?php foreach ($post as $p) {?>
+								<h5 class="card-title"><?php echo $p->titulo; ?></h5>
+								<p class="card-text"><?php echo $p->descricao; ?></p>
+								<p class="card-text"><small class="text-muted"><?php echo $p->datacadastro; ?></small></p> <?php } ?>
+							</div>
+						</div>
+						<div class="card">
+							<!--<img class="card-img-top" src="<?php echo base_url(); ?>assets/img/img1.jpg" alt="Imagem de capa do card">-->
+							<div class="card-body"> <?php foreach ($post as $p) {?>
+								<h5 class="card-title"><?php echo $p->titulo; ?></h5>
+								<p class="card-text"><?php echo $p->descricao; ?></p>
+								<p class="card-text"><small class="text-muted"><?php echo $p->datacadastro; ?></small></p> <?php } ?>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="card">
-					<!--<img class="card-img-top" src="<?php echo base_url(); ?>assets/img/img1.jpg" alt="Imagem de capa do card">-->
-					<div class="card-body">
-						<?php foreach ($post as $p) {?>
-						<h5 class="card-title"><?php echo $p->titulo; ?></h5>
-						<p class="card-text"><?php echo $p->descricao; ?></p>
-						<p class="card-text"><small class="text-muted"><?php echo $p->datacadastro; ?></small></p>
-						<?php } ?>
+
+				<div class="col-4">
+					<div class="border my-5">
+						<p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
 					</div>
-				</div>
-				<div class="card">
-					<!--<img class="card-img-top" src="<?php echo base_url(); ?>assets/img/img1.jpg" alt="Imagem de capa do card">-->
-					<div class="card-body">
-						<?php foreach ($post as $p) {?>
-						<h5 class="card-title"><?php echo $p->titulo; ?></h5>
-						<p class="card-text"><?php echo $p->descricao; ?></p>
-						<p class="card-text"><small class="text-muted"><?php echo $p->datacadastro; ?></small></p>
-						<?php } ?>
-					</div>
-				</div>
+				</div>	
+
 			</div>
 		</div>
 
--- application/views/menu.php
+++ application/views/menu.php
@@ -14,33 +14,34 @@
 
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-light bg-light py-3 px-5">
-		  <a class="navbar-brand" href="#">
-		    <img src="<?php echo base_url(); ?>assets/img/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
-		    My Blog
-		  </a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-		  <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-		    <ul class="navbar-nav">
-		      <li class="nav-item">
-		        <a class="nav-link" href="#">Home</a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="#">Posts</a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="#">Categorias</a>
-		      </li>
-		      <li class="nav-item dropdown">
-		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		          <img src="<?php echo base_url(); ?>assets/img/user.png" width="30" height="30" class="d-inline-block align-top" alt="">
-		        </a>
-		        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-		          <a class="dropdown-item" href="#">Meu perfil</a>
-		          <a class="dropdown-item" href="#">Sair</a>
-		        </div>
-		      </li>
-		    </ul>
-		  </div>
+			<a class="navbar-brand" href="#">
+				<img src="<?php echo base_url(); ?>assets/img/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+				My Blog
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Posts</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Categorias</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img src="<?php echo base_url(); ?>assets/img/user.png" width="30" height="30" class="d-inline-block align-top" alt="">
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="#">Meu perfil</a>
+							<a class="dropdown-item" href="#">Sair</a>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</nav>
+
