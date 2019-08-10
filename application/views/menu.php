10fedc506689952378aae8e3e9a930dba4e98af4
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-08-10 00:26:13 -0300

    alteração do botão dropdown

--- application/views/menu.php
+++ application/views/menu.php
@@ -2,18 +2,20 @@
 <html>
 	<head>
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
 		<!-- JQuery and Popper.js -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<!-- Bootstrap JavaScript -->
-		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
 
 		<title><?php echo $titulo; ?></title>
 	</head>
 
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light py-3 px-5">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top py-3 px-5">
 			<a class="navbar-brand" href="#">
 				<img src="<?php echo base_url(); ?>assets/img/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
 				My Blog
@@ -33,7 +35,7 @@
 						<a class="nav-link" href="#">Categorias</a>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							<img src="<?php echo base_url(); ?>assets/img/user.png" width="30" height="30" class="d-inline-block align-top" alt="">
 						</a>
 						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
