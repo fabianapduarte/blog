a71e9a4e63b4df9310f8f2327a3d807c80f29e15
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-08-14 11:06:01 -0300

    alteração na cor do rodapé

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,4 @@
-		<div class="text-center p-2 bg-light font-serif">
+		<div class="text-center p-2 font-serif" style="background-color: #F2F2F2">
 			<div>2019 &copy; My Blog</div>
 		</div>
 	</body>
