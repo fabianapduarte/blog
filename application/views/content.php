e155db2396c4c9ed06b39e3b2c1b93176a9d436f
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-08-14 08:59:56 -0300

    .

--- application/views/content.php
+++ application/views/content.php
@@ -1,4 +1,7 @@
-		<div class="slideshow">
+		<?php
+      if (!empty($carousel)) {
+    ?>
+    <div class="slideshow">
       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -19,7 +22,8 @@
           </div>
 
           <?php 
-              $item_class = ' ';
+                $item_class = ' ';
+              }
             }
            ?>
         </div>
@@ -39,33 +43,45 @@
 			<div class="row justify-content-between">
 				<div class="col-md-7">
 					<div class="display-4 my-5 font-serif">Novas publicações</div>
-          <?php foreach ($posts as $p) {?>
+          <?php
+          if (!empty($posts)) {
+            foreach ($posts as $p) {
+            
+          ?>
           <div class="card-group mb-5 font">
 						<div class="card">
 							<img class="card-img-top" src="<?php echo base_url(); ?>assets/img/<?php echo $p->img; ?>" alt="Imagem de capa do card">
 							<div class="card-body">
 								<h5 class="card-title font-serif"><?php echo $p->titulo; ?></h5>
-								<p class="card-text"><?php echo $p->descricao; ?></p>
+                <p class="card-text"><?php echo $p->descricao; ?></p>
+                <a href="#" class="btn btn-primary">Ver mais</a>
 							</div>
 						</div>
 					</div>
-          <?php } ?>
-
+          <?php
+            }
+          ?>
           <div>
             <a href="#" class="btn btn-primary mb-5">Todas as publicações</a>
           </div>
+          <?php
+          }
+          else {
+            echo "<p class='mb-5'>Não há novas publicações.</p>";
+          }
+          ?>
 				</div>
 
 				<div class="col-md-4">
 					<div class="about my-5 p-3">
             <?php foreach ($usuario as $user) { ?>
-						<h3 class="text-center mb-3 font-serif">Sobre mim</h3>
-            <div class="img-user">
-              <img src="<?php echo base_url(); ?>assets/img/<?php echo $user->img ?>" class="rounded-circle" width="250" height="250">
+						<h3 class="text-center font-serif">Sobre mim</h3>
+            <div class="img-user my-4">
+              <img src="<?php echo base_url(); ?>assets/img/<?php echo $user->img; ?>" class="w-100" height="270">
             </div>
             <div class="font-serif position-relative">
-              <h5 class="text-center"><?php echo $user->nome ?></h5>
-              <p><?php echo $user->sobre ?></p>
+              <h5 class="text-center"><?php echo $user->nome; ?></h5>
+              <p><?php echo $user->sobre; ?></p>
             </div>
           <?php } ?>
           </div>
