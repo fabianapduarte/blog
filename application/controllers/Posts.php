d3e2417da9c74a25208d3ead121debae93f937a2
= <=>
2019-08-29 18:04:13 -0300

    refatoração de controller, chamada de métodos do Post_Model

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -1,22 +1,25 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Posts extends CI_Controller {
+class Posts extends CI_Controller 
+{
 
-	public function todosOsPosts() {
-		$postagens['posts'] = $this->db->get('post')->result();
-		$data['usuario'] = $this->db->get('usuario', 1)->result();
+	public function todosOsPosts()
+	{
+		$postagens['posts'] = $this->Post_model->recuperarPost();
+		$data['usuario'] = $this->Post_model->recuperarUsuario();
 		$data['titulo'] = 'Todas as publicações';
 		$data['pagina'] = 'Posts';
 
-		$this->load->view('menu', $data);
+		$this->load->view('menu', $data);;
 		$this->load->view('publicacoes', $postagens, $data);
 		$this->load->view('footer');
 	}
 
-    public function novoPost() {
-        $categorias['categoria'] = $this->db->get('categoria')->result();
-        $data['usuario'] = $this->db->get('usuario', 1)->result();
+    public function novoPost() 
+    {
+    	$categorias['categoria'] = $this->Post_model->recuperarCategoria();
+      	$data['usuario'] = $this->Post_model->recuperarUsuario();
 		$data['titulo'] = 'Nova Publicação';
         $data['pagina'] = 'Posts';
         
