c84a415d42a4c496f12b9a430c29c7f615e47c5b
joao paulo <joaopaulopmedeiros@gmail.com>
2019-08-29 22:24:03 -0300

    inserção de dados no banco

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -2,6 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Post_model extends CI_Model{
+	
+	public $titulo;
+	public $categoria_id;
+	public $descricao;
+	public $texto;
+	public $img;
+
 	public function recuperarPost() //método para recuperação de tabela post 
 	{
 		return $postagens = $this->db->get('post')->result();
@@ -13,5 +20,15 @@ class Post_model extends CI_Model{
 	public function recuperarCategoria() //método para recuperação de tabela categoria
 	{
 		return $categorias = $this->db->get('categoria')->result();
+	}
+	public function inserirPost(){
+		$data = array(
+			"titulo" => $this->titulo,
+			"categoria_id" => $this->categoria_id,
+			"descricao" => $this->descricao,
+			"texto" => $this->texto,
+			"img" => $this->img
+		);
+		return $this->db->insert('post',$data);
 	}
 }
