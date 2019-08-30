264eb208a1aa8444c194be67b60bdbe2ecdcafd7
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2019-08-29 21:59:43 -0300

    .

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang='pt-br'>
 	<head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
 		<!-- JQuery and Popper.js -->
