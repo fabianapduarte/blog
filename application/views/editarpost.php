06f8031a971a26fc1013d04640d3e47fec1845de
Fabiana Pereira Duarte <fabiana.duarte@academico.ifrn.edu.br>
2019-09-12 17:31:25 -0300

    edição dos campos de editar post

--- application/views/editarpost.php
+++ application/views/editarpost.php
@@ -1,34 +1,42 @@
     <div class="container">
       <div class="font-serif display-4 text-center my-4">Nova Publicação</div>
       
-      <form class="my-5" method="post" action="<?=base_url();?>posts/salvar">
+      <form class="my-5" method="post" action="<?=base_url();?>posts/atualizar">
         <div class="form-row">
           <div class="form-group col-md-8">
             <label for="inputTitulo">Título</label>
-            <input type="text" class="form-control" name="inputTitulo" placeholder="Título da publicação" required>
+            <input type="text" class="form-control" name="inputTitulo" placeholder="Título da publicação" value="<?= $postagens['titulo']?>" required>
           </div>
 
           <div class="form-group col-md-4">
             <label for="inputCategoria">Categoria</label>
             <select name="inputCategoria" class="form-control" required>
-              <option selected>Escolher...</option>
               <?php 
                 foreach ($categoria as $c) {
+                  if ($c->id == $postagens['categoria_id']) {
               ?>
-              <option value="<?php echo $c->id; ?>"><?php echo $c->nome; ?></option>
-              <?php } ?>
+                    <option select value="<?php echo $postagens['categoria_id']; ?>"><?php echo $c->nome; ?></option>
+              <?php 
+                  }
+                  else {
+              ?>
+                    <option value="<?php echo $c->id; ?>"><?php echo $c->nome; ?></option>
+              <?php 
+                  }
+                }
+              ?>
             </select>
           </div>
         </div>
 
         <div class="form-group">
           <label for="inputDescricao">Descrição</label>
-          <input type="text" class="form-control" name="inputDescricao" placeholder="Descrição da publicação (opcional)">
+          <input type="text" class="form-control" name="inputDescricao" placeholder="Descrição da publicação (opcional)" value="<?= $postagens['descricao']?>">
         </div>
 
         <div class="form-group">
           <label for="inputText">Texto</label><br>
-          <textarea class="form-control" name="inputText" rows="20" required></textarea>
+          <textarea class="form-control" name="inputText" rows="20" required><?= $postagens['texto']?></textarea>
         </div>
 
         <div class="form-group">
